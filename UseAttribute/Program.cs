e9b9f061b7d582a69ae02cf2a01ce3273d86ef19
root <2485751840@qq.com>
2025-03-30 11:47:03 +0800

    特性

---
+++ UseAttribute/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace UseAttribute
+{
+    /// <summary>
+    /// 使用特性
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
