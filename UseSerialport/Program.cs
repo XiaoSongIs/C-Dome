fcc539b73b96e0e259b3398460b066bef81059c4
root <2485751840@qq.com>
2025-04-21 22:05:13 +0800

    串口的使用

---
+++ UseSerialport/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.IO.Ports;
+
+namespace UseSerialport
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 使用串口
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            SerialPort serialPort = new SerialPort();
+            serialPort.PortName = "COM3";          // 设置串口名称 
+            serialPort.BaudRate = 9600;           // 波特率
+            serialPort.Parity = Parity.None;      // 校验位
+            serialPort.DataBits = 8;              // 数据位
+            serialPort.StopBits = StopBits.One;   // 停止位
+            serialPort.Handshake = Handshake.None;
+            serialPort.Open();
+
+
+            
+            serialPort.WriteLine("\r\n");
+            serialPort.DataReceived += (sender, e) =>
+            {
+                try
+                {
+                    string data = serialPort.ReadExisting();
+                    Console.WriteLine("收到ESP8266消息：" + data);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("读取串口数据出错：" + ex.Message);
+                }
+            };
+
+
+            while (true)
+            {
+                Console.WriteLine("输入");
+                string input = Console.ReadLine();
+                serialPort.WriteLine(input);
+            }
+
+            Console.ReadLine();
+
+            //// 获取当前可用的串口
+            //string[] str = SerialPort.GetPortNames();
+
+        }
+    }
+}
---
+++ UseThree/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace UseThree
+#nullable disable
+{
+    /// <summary>
+    /// 使用树
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class Tree<T>
+    {
+        public class TreeNode
+        {
+            public T value;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(T t)
+            {
+                this.value = t;
+                left = null;
+                right = null;
+            }
+        }
+
+        public TreeNode root;  // 父节点
+
+        private TreeNode InsetRe(TreeNode node,T value)
+        {
+            //if(node == null)
+            //{
+            //    return new TreeNode(value);
+            //}
+            //if (int.Parse(value.ToString()) < int.Parse(node.left.ToString()))
+            //{
+            //    node.Left = InsetRe(node.Left, value);
+            //}
+            return node;
+        }
+
+    }
+    
+}
